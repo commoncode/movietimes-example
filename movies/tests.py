02168696c0fdfb1247207054618e309020ddeb00
Hardik Shah <hardik.shah@reece.com.au>
2017-05-29 23:03:39 +1000

    Added reverse function to remove URL hardcoding

--- movies/tests.py
+++ movies/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 from .factories import ActorFactory, MovieFactory
 from .models import Movie
@@ -30,10 +31,10 @@ class PaginatedMovieTestCase(TestCase):
         movies = Movie.objects.all().order_by('-release_date', 'title')[:5]
         movie_titles = [m.title for m in movies]
 
-        response_page1 = self.client.get('/movies/recent/?page=').json()
+        response_page1 = self.client.get(reverse('movies:recent-movies')+'?page=').json()
         self.assertListEqual(response_page1, movie_titles)
 
-        response_page2 = self.client.get('/movies/recent/').json()
+        response_page2 = self.client.get(reverse('movies:recent-movies')).json()
         self.assertListEqual(response_page2, movie_titles)
 
     def test_url_with_page_offsets_to_correct_page(self):
@@ -42,14 +43,14 @@ class PaginatedMovieTestCase(TestCase):
         movies = Movie.objects.all().order_by('-release_date', 'title')[20:25]
         movie_titles = [m.title for m in movies]
 
-        response_page = self.client.get('/movies/recent/?page=5').json()
+        response_page = self.client.get(reverse('movies:recent-movies')+'?page=5').json()
         self.assertListEqual(response_page, movie_titles)
 
     def test_url_with_page_out_of_range_display_404(self):
 
         [MovieFactory() for _ in range(100)]
 
-        response = self.client.get('/movies/recent/?page=21')
+        response = self.client.get(reverse('movies:recent-movies')+'?page=21')
         self.assertEqual(response.status_code, 404)
 
 
@@ -58,12 +59,12 @@ class RecentMoviesTestCase(TestCase):
     def test_max_number_of_movies_returned_are_5(self):
 
         [MovieFactory() for _ in range(10)]
-        response = self.client.get('/movies/recent/')
+        response = self.client.get(reverse('movies:recent-movies'))
         response_json = response.json()
         self.assertLessEqual(len(response_json), 5)
 
         [MovieFactory() for _ in range(3)]
-        response = self.client.get('/movies/recent/')
+        response = self.client.get(reverse('movies:recent-movies'))
         response_json = response.json()
         self.assertLessEqual(len(response_json), 5)
 
@@ -78,7 +79,7 @@ class RecentMoviesTestCase(TestCase):
         ]
         movie_titles = [m.title for m in reversed(movies)]
 
-        response = self.client.get('/movies/recent/')
+        response = self.client.get(reverse('movies:recent-movies'))
         response_json = response.json()
 
         self.assertListEqual(movie_titles, response_json)
@@ -94,7 +95,7 @@ class RecentMoviesTestCase(TestCase):
         ]
         movie_titles = [m.title for m in reversed(movies)]
 
-        response = self.client.get('/movies/recent/')
+        response = self.client.get(reverse('movies:recent-movies'))
         response_json = response.json()
 
         self.assertListEqual(movie_titles, response_json)
@@ -104,7 +105,7 @@ class RecentMoviesTestCase(TestCase):
         movies = [MovieFactory() for _ in range(5)]
         movie_titles = [m.title for m in movies]
 
-        response = self.client.get('/movies/recent/')
+        response = self.client.get(reverse('movies:recent-movies'))
         response_json = response.json()
 
         for movie_title in movie_titles:
@@ -114,32 +115,32 @@ class RecentMoviesTestCase(TestCase):
             self.assertIn(movie_title, movie_titles)
 
     def test_recent_movies_returns_a_list(self):
-        response = self.client.get('/movies/recent/')
+        response = self.client.get(reverse('movies:recent-movies'))
         response_json = response.json()
         self.assertIsInstance(response_json, list)
 
     def test_recent_movies_returns_json(self):
-        response = self.client.get('/movies/recent/')
+        response = self.client.get(reverse('movies:recent-movies'))
         self.assertEqual(response['content-type'], 'application/json')
 
     def test_non_get_requests_return_400(self):
-        response = self.client.post('/movies/recent/')
+        response = self.client.post(reverse('movies:recent-movies'))
         self.assertEqual(response.status_code, 400)
 
-        response = self.client.put('/movies/recent/')
+        response = self.client.put(reverse('movies:recent-movies'))
         self.assertEqual(response.status_code, 400)
 
-        response = self.client.delete('/movies/recent/')
+        response = self.client.delete(reverse('movies:recent-movies'))
         self.assertEqual(response.status_code, 400)
 
-        response = self.client.patch('/movies/recent/')
+        response = self.client.patch(reverse('movies:recent-movies'))
         self.assertEqual(response.status_code, 400)
 
-        response = self.client.head('/movies/recent/')
+        response = self.client.head(reverse('movies:recent-movies'))
         self.assertEqual(response.status_code, 400)
 
     def test_get_best_movies_url(self):
-        response = self.client.get('/movies/recent/')
+        response = self.client.get(reverse('movies:recent-movies'))
         self.assertEqual(response.status_code, 200)
 
 
@@ -149,20 +150,20 @@ class BestMoviesTestCase(TestCase):
 
         [MovieFactory() for _ in range(140)]
 
-        response_page1 = self.client.get('/movies/best/?page=1').json()
-        response_page2 = self.client.get('/movies/best/?page=2').json()
+        response_page1 = self.client.get(reverse('movies:best-movies')+'?page=1').json()
+        response_page2 = self.client.get(reverse('movies:best-movies')+'?page=2').json()
 
         self.assertListEqual(response_page1, response_page2)
 
     def test_max_number_of_movies_returned_are_5(self):
 
         [MovieFactory() for _ in range(10)]
-        response = self.client.get('/movies/best/')
+        response = self.client.get(reverse('movies:best-movies'))
         response_json = response.json()
         self.assertLessEqual(len(response_json), 5)
 
         [MovieFactory() for _ in range(3)]
-        response = self.client.get('/movies/best/')
+        response = self.client.get(reverse('movies:best-movies'))
         response_json = response.json()
         self.assertLessEqual(len(response_json), 5)
 
@@ -177,7 +178,7 @@ class BestMoviesTestCase(TestCase):
         ]
         movie_titles = [m.title for m in reversed(movies)]
 
-        response = self.client.get('/movies/best/')
+        response = self.client.get(reverse('movies:best-movies'))
         response_json = response.json()
 
         self.assertListEqual(movie_titles, response_json)
@@ -193,7 +194,7 @@ class BestMoviesTestCase(TestCase):
         ]
         movie_titles = [m.title for m in reversed(movies)]
 
-        response = self.client.get('/movies/best/')
+        response = self.client.get(reverse('movies:best-movies'))
         response_json = response.json()
 
         self.assertListEqual(movie_titles, response_json)
@@ -203,7 +204,7 @@ class BestMoviesTestCase(TestCase):
         movies = [MovieFactory() for _ in range(5)]
         movie_titles = [m.title for m in movies]
 
-        response = self.client.get('/movies/best/')
+        response = self.client.get(reverse('movies:best-movies'))
         response_json = response.json()
 
         for movie_title in movie_titles:
@@ -213,30 +214,30 @@ class BestMoviesTestCase(TestCase):
             self.assertIn(movie_title, movie_titles)
 
     def test_best_movies_returns_a_list(self):
-        response = self.client.get('/movies/best/')
+        response = self.client.get(reverse('movies:best-movies'))
         response_json = response.json()
         self.assertIsInstance(response_json, list)
 
     def test_get_best_movies_url(self):
-        response = self.client.get('/movies/best/')
+        response = self.client.get(reverse('movies:best-movies'))
         self.assertEqual(response.status_code, 200)
 
     def test_best_movies_returns_json(self):
-        response = self.client.get('/movies/best/')
+        response = self.client.get(reverse('movies:best-movies'))
         self.assertEqual(response['content-type'], 'application/json')
 
     def test_non_get_requests_return_400(self):
-        response = self.client.post('/movies/best/')
+        response = self.client.post(reverse('movies:best-movies'))
         self.assertEqual(response.status_code, 400)
 
-        response = self.client.put('/movies/best/')
+        response = self.client.put(reverse('movies:best-movies'))
         self.assertEqual(response.status_code, 400)
 
-        response = self.client.delete('/movies/best/')
+        response = self.client.delete(reverse('movies:best-movies'))
         self.assertEqual(response.status_code, 400)
 
-        response = self.client.patch('/movies/best/')
+        response = self.client.patch(reverse('movies:best-movies'))
         self.assertEqual(response.status_code, 400)
 
-        response = self.client.head('/movies/best/')
+        response = self.client.head(reverse('movies:best-movies'))
         self.assertEqual(response.status_code, 400)
