733557eaa1ffb07fb4c3591ee1769918e60dffbe
Brenton Cleeland <brenton@brntn.me>
2017-05-29 11:02:25 +1000

    👷 Add our first TDD tests

--- movies/tests.py
+++ movies/tests.py
@@ -24,10 +24,47 @@ class FactoryTestCase(TestCase):
 
 class BestMoviesTestCase(TestCase):
 
+    def test_best_movies_returns_a_list_of_movie_titles(self):
+        # create 5 random movies
+        movies = [MovieFactory() for _ in range(140)]
+        movie_titles = [m.title for m in movies]
+
+        response = self.client.get('/movies/best/')
+        response_json = response.json()
+
+        for movie_title in movie_titles:
+            self.assertIn(movie_title, response_json)
+
+        for movie_title in response_json:
+            self.assertIn(movie_title, movie_titles)
+
+        self.assertEqual(len(movies), len(response_json))
+
+    def test_best_movies_returns_a_list(self):
+        response = self.client.get('/movies/best/')
+        response_json = response.json()
+        self.assertIsInstance(response_json, list)
+
     def test_get_best_movies_url(self):
         response = self.client.get('/movies/best/')
-        self.assertIs(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
     def test_best_movies_returns_json(self):
         response = self.client.get('/movies/best/')
-        self.assertIs(response['content-type'], 'application/json')
+        self.assertEqual(response['content-type'], 'application/json')
+
+    def test_non_get_requests_return_400(self):
+        response = self.client.post('/movies/best/')
+        self.assertEqual(response.status_code, 400)
+
+        response = self.client.put('/movies/best/')
+        self.assertEqual(response.status_code, 400)
+
+        response = self.client.delete('/movies/best/')
+        self.assertEqual(response.status_code, 400)
+
+        response = self.client.patch('/movies/best/')
+        self.assertEqual(response.status_code, 400)
+
+        response = self.client.head('/movies/best/')
+        self.assertEqual(response.status_code, 400)
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,14 @@
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponseBadRequest
+
+from .models import Movie
 
 
 def best_movies(request):
-    return HttpResponse()
+    if request.method.upper() != 'GET':
+        return HttpResponseBadRequest()
+
+    movie_titles_list = []
+    for movie in Movie.objects.all():
+        movie_titles_list.append(movie.title)
+
+    return JsonResponse(movie_titles_list, safe=False)
